e960eb2fced0900cf785965a5fed61807af67c9c
brianschnee <brian.a.schnee@gmail.com>
2024-08-14 01:13:50 -0400

    feat: test enum variant messages

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod error;
+mod main_tests;
 
 use std::{
     fs::{self, OpenOptions},
---
+++ src/main_tests.rs
@@ -0,0 +1,29 @@
+#[cfg(test)]
+mod tests {
+    use crate::error::Error;
+
+    #[test]
+    fn error_variants_return_proper_messages() {
+        let pairs: &[(Error, &str)] = &[
+            (Error::CannotBuildPath, "cannot construct path"),
+            (
+                Error::CannotFindDir("parent".to_string()),
+                "cannot find parent directory",
+            ),
+            (
+                Error::CannotCreateDir("./til/notes".to_string()),
+                "cannot create ./til/notes directory",
+            ),
+            (
+                Error::CannotProcessArgs,
+                "cannot process command-line arguments",
+            ),
+            (String::from("custom message").into(), "custom message"),
+            (Error::default(), "something wrong happened"),
+        ];
+
+        pairs
+            .iter()
+            .for_each(|(err, msg)| assert_eq!(err.message(), msg.to_string()));
+    }
+}
